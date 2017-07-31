79d6f56d18ee5d7044df56b2550057dd3a0237b5
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-31 01:08:46 -0300

    Agregando modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
 	});
 
 	miJson.forEach(function(e){
-		$('.elemento').append('<div class="col s2 m2 l2 xl2 caja"><img class="materialboxed modal-trigger" href="#modal1" src="dist/img/'+ e.image_url +'">'+ '<br>' + '<i class="fa fa-check grey-text right" aria-hidden="true"> 6</i>'+'<i class="fa fa-thumb-tack grey-text right" aria-hidden="true"> 36,6k</i>'+'<h6 class="titulo left-align">'+ e.title+'</h6><p class= "descripcion left-align">'+ e.description+'</p><p class= "user left-align">'+ e.user + '&nbsp<span>#</span>' + e.hashtag + '</p></div');	
+		$('.elemento').append('<div class="col s2 m2 l2 xl2 caja"><img class="responsive-img materialboxed modal-trigger" href="#modal1" src="dist/img/'+ e.image_url +'">'+ '<br>' + '<h6 class="titulo left-align">'+ e.title+'</h6><i class="right material-icons">check_circle</i>'+'<i class="right material-icons">save</i>36,6k'+ '<p class= "descripcion left-align">'+ e.description+'</p><p class= "user left-align">'+ e.user + '&nbsp<span>#</span>' + e.hashtag + '</p></div');	
 		console.log(e);
 	});
 
 	miJson.forEach(function(e){
-		$('.contenedor-modal').append('<h6 class="titulo left-align">'+ e.title+'</h6>' + '<br>' + '<img src="dist/img/'+ e.id +'.jpg">' + '<div class="row"><a href="#"><i class="fa fa-upload" aria-hidden="true"></i></a><a href="#"><i class="fa fa-check" aria-hidden="true"></i></a><a href="#"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a><a class="btn red right"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Guardar</a></div></div>' + '<div class="row"><div class="col m6 l6 xl6"><p class"user left-align">'+ e.user + '&nbsp<span>#</span>' + e.hashtag + '</p></div>' + '<div class="col m6 l6 xl6"><a class="btn btn-leerlo right">Leerlo</a></div></div>' + '<p class="descripcion left-align">' + e.description + '</p>');
+		$('.contenedor-modal').append('<div class="row"><a href="#"><i class="material-icons">publish</i></a><a href="#"><i class="material-icons">check_circle</i></a><a href="#"><i class="material-icons">more_horiz</i></a><a class="btn red right"><i class="material-icons">save</i>Guardar</a><h6 class="titulo left-align">'+ e.title +'</h6>' + '<br>' + '<img  class=" responsive-img imagenes-modal" src="dist/img/'+ e.id +'.jpg">' + '<a href="#"><i class="material-icons">unarchive</i></a><a href="#"><i class="material-icons">check_circle</i></a><a href="#"><i class="material-icons">more_horiz</i></a><a class="btn red right"><i class="material-icons">save</i>Guardar</a></div></div>' + '<div class="row"><div class="col m6 l6 xl6"><p class"user left-align">'+ e.user + '&nbsp<span>#</span>' + e.hashtag + '</p></div>' + '<div class="col m6 l6 xl6"><a class="btn right"><i class="material-icons">library_books</i>Leerlo</a></div></div>' + '<p class="descripcion left-align">' + e.description + '</p>');
 	})
 
 	$('.modal').modal();
